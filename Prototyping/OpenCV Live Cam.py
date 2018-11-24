2cc67de00df6bda04509c8fda902d49cadda25ff
TinkerTavern <43844772+TinkerTavern@users.noreply.github.com>
2018-11-24 21:51:57 +0000

    Initial Push
    + Added Prototyping folder
    + Added some OpenCV test programs, including one which works for the webcam
    + Added a documentation file

---
+++ Prototyping/OpenCV Live Cam.py
@@ -0,0 +1,53 @@
+import cv2
+import sys
+import time as t
+import numpy as np
+
+cascPath = "data/haarcascade_frontalface_alt.xml"#sys.argv[1]
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+timearray = np.zeros(1000)
+index = 0
+detection = 0
+limit = 30
+while True:
+    start = t.time()
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+    
+    # Draw a rectangle around the faces
+    for (x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
+        if width > 100:
+            detection += 1
+            if detection > limit:
+                print("Face detected!")
+        else:
+            detection = 0
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    end = t.time()
+    if index < 1000:
+        timearray[index] = end-start
+    else:
+        break
+    index += 1
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
+print(1/(np.average(timearray)))
