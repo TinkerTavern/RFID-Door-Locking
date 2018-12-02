0fd5c6e48e3e123b415db287f84dcab67e4537f9
JakeUni <jakemifsud39@gmail.com>
2018-12-02 13:12:01 +0000

    Clean up and more coms i guess
    +cleaner code
    +many more comments
    +made slave count incorrect answers
    +fixed button interrupts
    + fixed restart function - no more setup used
    + added return "r" to slave to be restart
    +Start alarm function
    - alot of random stuff ngl
    + Comms setup text file
    -will to live

--- Prototyping/Screen_RFID/Screen_RFID.ino
+++ Prototyping/Screen_RFID/Screen_RFID.ino
@@ -12,14 +12,14 @@ int answerButton2 = 3;
 //bluetooth libraries
 #include <SoftwareSerial.h>
 //initializing bluetooth connection
-SoftwareSerial mySerial(4, 5); 
+SoftwareSerial mySerial(4, 5);
 // TFT Screen pins, configured to be different
 #define cs   8
 #define dc   7
-#define rst  6  
+#define rst  6
 // RFID Pins, not changed
-#define RST_PIN         9           
-#define SS_PIN          10       
+#define RST_PIN         9
+#define SS_PIN          10
 
 String RFIDValid = "f";
 String binaryAnswer = "";
@@ -31,274 +31,322 @@ MFRC522 rfid(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 MFRC522::MIFARE_Key key;
 
 millisDelay timer;
-// Init array that will store new NUID 
+// Init array that will store new NUID
 byte nuidPICC[4];
 
 char sensorPrintout[13];
 bool faceDetected = false;
 String timeRemaining;
-int invalidRFID = 0;
-int invalidPassword = 0;
 
+// a boolean value to store whether the buttons have been pressed
+bool buttonsPressed = true; //starts as true as we dont want the user to use buttons at the start
+
+//Setup function initializing variables and starting connections.
 void setup() {
   TFTscreen.begin();
-  
+
   mySerial.begin(38400);
   Serial.begin(9600); // Initialize serial communications with the PC
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
   SPI.begin();        // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
+  rfid.PCD_Init(); // Init MFRC522
   for (byte i = 0; i < 6; i++) {
-      key.keyByte[i] = 0xFF;
+    key.keyByte[i] = 0xFF;
   }
   attachInterrupt(0, answer1, FALLING); //specifying which interrupt pins correspond to which pin, and specifying the ISRs
   attachInterrupt(1, answer2, FALLING);
 
-  pinMode(answerButton1, OUTPUT); //set player buttons as inputs
-  pinMode(answerButton2, OUTPUT);
+  pinMode(answerButton1, INPUT); //set player buttons as inputs
+  pinMode(answerButton2, INPUT);
 
   TFTscreen.background(255, 0, 0);
-  TFTscreen.stroke(255,255,255);
+  TFTscreen.stroke(255, 255, 255);
   TFTscreen.text("Waiting for face", 5, 40); // Replaces the text with an empty string
-//  timer.start(30000);
-//  updateTimer();
+  //  timer.start(30000);
+  //  updateTimer();
 }
 
-
+//main function which will be looped through
 void loop() {
-  if (!faceDetected) { 
+  //checking if a face has been detected from the slave arduino
+  if (!faceDetected) {
     String newString;
-    if (Serial.available()) {
-      while (Serial.available()) {
-        delay(10);  //small delay to allow input buffer to fill
-        char c = Serial.read();  //gets one byte from serial buffer
-        newString += c;
-      } //makes the string readString
-    
+    newString = "True";
+     if (Serial.available()) {
+    while (Serial.available()) {
+      delay(10);  //small delay to allow input buffer to fill
+      char c = Serial.read();  //gets one byte from serial buffer
+      newString += c;
+    } //makes the string readString
+    //if a face has been detected, start the timer
     if (newString.length() > 0) {
-      if (newString == "True"){
+      if (newString == "True") {
         faceDetected = true;
         TFTscreen.background(255, 0, 0);
-        TFTscreen.stroke(255,255,255);
+        TFTscreen.stroke(255, 255, 255);
         TFTscreen.text("Please scan RFID card", 5, 40); // Replaces the text with an empty string
         if (!timer.isRunning()) timer.start(30000);
         if (timer.isRunning()) timer.restart();
-        //timer.restart();
+
         updateTimer();
       }
     }
   }
   }
+  //check the timer to see if it has run out, if not then update said timer.
   else {
-  checkTimer();
-  updateTimer();
-  if (RFIDValid == "f"){
-  String sensorVal;
-// Look for new cards
-  if (! rfid.PICC_IsNewCardPresent())
-    return;
-  
-  // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
-    return;
-
-  //Serial.print(F("PICC type: "));
-  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-  //Serial.println(rfid.PICC_GetTypeName(piccType));
-
-  // Check is the PICC of Classic MIFARE type
-  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-    //Serial.println(F("Your tag is not of type MIFARE Classic."));
-    return;
-  }
-
-  // Store NUID into nuidPICC array
-  for (byte i = 0; i < 4; i++) {
-    nuidPICC[i] = rfid.uid.uidByte[i];
-  }
-
- // TFTscreen.stroke(255,0,0); // Clears the previous text
- // TFTscreen.text(sensorPrintout, 10, 70); // Replaces the text with an empty string
-
-  sensorVal = byteToStringArray(rfid.uid.uidByte, rfid.uid.size);
-
-
-  // Halt PICC
-  rfid.PICC_HaltA();
-
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
-
-  //Serial.println(sensorVal);
-  
-  sensorVal.toCharArray(sensorPrintout, 13); // Converts the string to a char array for display
-  mySerial.write(sensorPrintout); // Sends the RFID tag for verification
-  delay(200); // Delay to ensure the slave's response is finished before checking
-  RFIDValid = recieveCommand();
-
-  if (RFIDValid == "t"){
-    TFTscreen.background(255, 0, 0);
-    TFTscreen.stroke(255,255,255);
-    TFTscreen.text("Access Granted", 5, 40); // Replaces the text with an empty string
-    updateScreen("Access Granted",14 , 5, 40);
-    //mySerial.write("Dont Kill");
-    tone(A0, 3000, 500); // Correct tag
-    delay(1000);
     checkTimer();
-    updateScreen("What is the answer to the universe?",35 , 5, 20);
-    TFTscreen.text("1", 5, 40); // Replaces the text with an empty string
-    TFTscreen.text("0", 5, 60); // Replaces the text with an empty string
-    buttonsOn();
+    updateTimer();
+    //if they have not scanned a correct RDIF tag
+    if (RFIDValid == "f") {
+      String sensorVal;
+      // Look for new cards
+      if (! rfid.PICC_IsNewCardPresent())
+        return;
 
-  }else{
-    updateScreen("Access Denied",13 , 5, 40);
-    //mySerial.write("Kill");
-    tone(A0, 100, 500); // Incorrect tag
-    invalidRFID++;
-  }
-  }else{
-   delay(10);
-    if (binaryAnswer.length() == 8){
-//      String Answer="";
+      // Verify if the NUID has been readed
+      if ( ! rfid.PICC_ReadCardSerial())
+        return;
 
-      char Answer[8];
-      for(int i = 0 ; i < 8; i++){
-        Answer[i] = binaryAnswer.charAt(i);
-        Answer[i+1] = '\0';
+      //Serial.print(F("PICC type: "));
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      //Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // Check is the PICC of Classic MIFARE type
+      if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+          piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+          piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+        //Serial.println(F("Your tag is not of type MIFARE Classic."));
+        return;
       }
-      Serial.println(Answer);
-      mySerial.write(Answer);
-      delay(300);
-    }
-   
-    if(mySerial.available()){ 
-      String result = recieveCommand();
-      binaryAnswer = "";
-      if (result == "t"){
-        updateScreen("Door Opened",11 , 5, 40);
-        delay(500);
-        tone(A0, 300, 500);
-        delay(200);
-        tone(A0, 900, 500);
-        delay(200);
-        tone(A0, 1200, 500);
-        restartProgram();
-      }else{
+
+      // Store NUID into nuidPICC array
+      for (byte i = 0; i < 4; i++) {
+        nuidPICC[i] = rfid.uid.uidByte[i];
+      }
+
+      // TFTscreen.stroke(255,0,0); // Clears the previous text
+      // TFTscreen.text(sensorPrintout, 10, 70); // Replaces the text with an empty string
+
+      sensorVal = byteToStringArray(rfid.uid.uidByte, rfid.uid.size);
+
+
+      // Halt PICC
+      rfid.PICC_HaltA();
+
+      // Stop encryption on PCD
+      rfid.PCD_StopCrypto1();
+
+
+      sensorVal.toCharArray(sensorPrintout, 13); // Converts the string to a char array for display
+      mySerial.write(sensorPrintout); // Sends the RFID tag for verification
+      delay(200); // Delay to ensure the slave's response is finished before checking
+      RFIDValid = recieveCommand();
+
+      //if they have scanned a correct RFID tag then we need to swap to question mode
+      if (RFIDValid == "t") {
+        updateScreen("Access Granted", 15, 5, 40);
+        tone(A0, 3000, 500); // Correct tag
         delay(1000);
-        binaryAnswer = "";
-        updateScreen("Wrong Answer! Try Again",23 , 5, 40);
-        invalidPassword ++;
-        delay(1000);
-        updateScreen("What is the answer to the universe?",35 , 5, 20);
-        TFTscreen.text("1", 50, 40); // Replaces the text with an empty string
-        TFTscreen.text("0", 50, 60); // Replaces the text with an empty string
-        binaryAnswer = "";
-       // mySerial.write("kill");
+        checkTimer(); //checking if the time is up
+        updateScreen("What is the answer to the universe?", 37, 5, 20);
+        TFTscreen.text("1", 5, 40); // adding the button values onto the screen
+        TFTscreen.text("0", 5, 60);
+        buttonsPressed = false;
+
+      }
+      else if (RFIDValid == "r") {
+        //if the return was "r" restart the program after the alarm is turned on
+        //as there have been 3 incorrect rfid scans
+        startAlarm();
+      }
+      else {
+        //else, if the RFID was not valid then, display access denied and play an alarm
+        updateScreen("Access Denied", 15, 5, 40);
+        tone(A0, 100, 500); // Incorrect tag
       }
     }
-    
-        buttonsOn();
-    
-   }
+    //if the RFID tag has been answered as correct, then check the question answer
+    else {
+      delay(10);
+      //if the answer is of length 8 then it is a full answer and needs to be evaluated
+      if (binaryAnswer.length() == 8) {
+        //      String Answer="";
+
+        char Answer[8];
+        //loops through the answer and adds it to a char array
+        //to be writted to the slave arduino for checking
+        for (int i = 0 ; i < 8; i++) {
+          Answer[i] = binaryAnswer.charAt(i);
+          Answer[i + 1] = '\0';
+        }
+        Serial.println(Answer);
+        mySerial.write(Answer);
+        //a delay of 300 to allow the slave arduino to recieve,process and send a response
+        delay(300);
+      }
+      //if there is a message from the slave
+      if (mySerial.available()) {
+        String result = recieveCommand();
+        binaryAnswer = "";
+        //if the slave returned true then the "door" will be opened
+        //a nice tune will also be played and the program will restart after some time
+        if (result == "t") {
+          updateScreen("Door Opened", 12, 5 , 40);
+          delay(500);
+          tone(A0, 300, 500);
+          delay(200);
+          tone(A0, 900, 500);
+          delay(200);
+          tone(A0, 1200, 500);
+          restartProgram();
+        }
+        else if (result == "r") {
+          //if the return was "r" restart the program after the alarm is turned on
+          //as there have been 3 incorrect question answers
+          startAlarm();
+        }
+        else {
+          //if the slave returns anything other then true
+          // out put that the answer is wrong and reset the questions
+          delay(1000);
+          binaryAnswer = "";
+          updateScreen("Wrong Answer! Try Again", 25, 5, 40);
+          delay(1000);
+          //re writing the screen
+          updateScreen("What is the answer to the universe?", 37 , 5, 20);
+          TFTscreen.text("1", 50, 40); // Replaces the text with an empty string
+          TFTscreen.text("0", 50, 60); // Replaces the text with an empty string
+          binaryAnswer = "";
+        }
+
+
+      }
+      // turn the buttons back on
+
+      if (faceDetected == true) {
+        buttonsPressed = false;
+      }
+    }
   }
 }
 
 
+//this function will take the input from system.read and turn it int oa string array
 
 String byteToStringArray(byte *buffer, byte bufferSize) {
   String arr[bufferSize];
-  for (byte i=0; i < bufferSize; i++) arr[i] = String(buffer[i], HEX);
+  //loop through for each integer value and store it in a new array
+  for (byte i = 0; i < bufferSize; i++) {
+    arr[i] = String(buffer[i], HEX);
+  }
+  //pass the new array to the intArrayToString function which will return a string
   String str = intArrayToString(arr, bufferSize);
   return str;
 }
 
-
-String intArrayToString(String *intArray, byte bufferSize){
-
-    String fullString;
-    for (int j=0; j < bufferSize; j++) {
-      fullString.concat(intArray[j]);
-      fullString.concat(" ");
-    }
-    return fullString;
+//this is used by the above function, to turn a string into an integer
+String intArrayToString(String *intArray, byte bufferSize) {
+  //for each Value in the array we loop through and add this to a new string which we then return
+  String fullString;
+  for (int j = 0; j < bufferSize; j++) {
+    fullString.concat(intArray[j]);
+    fullString.concat(" ");
+  }
+  return fullString;
 }
+
+
 void answer1() { //ISR for the answer 1 button
- buttonsOff();
- binaryAnswer+= "1";
+  if (buttonsPressed == false) {
+    binaryAnswer += "1";
+    buttonsPressed = true;
+  }
 }
 void answer2() { //ISR for the answer 2 button
- buttonsOff(); 
- binaryAnswer+= "0";
-}
-void buttonsOff(){
- pinMode(answerButton1, OUTPUT); //set player buttons as outputs
- pinMode(answerButton2, OUTPUT);
-}
-void buttonsOn(){
- pinMode(answerButton1, INPUT); //set player buttons as inputs
- pinMode(answerButton2, INPUT);
-}
-
-
-String recieveCommand() {
-    String command;
-    delay(100); //
-    if (mySerial.available()) {
-      char c = mySerial.read();  //gets one byte from serial buffer
-      command += c;
-    } //makes the string readString
-
-    if (command.length() > 0) {
-      //Serial.println(test); //prints string to serial port out
-      //test = ""; //clears variable for new input
-      return command; // Returns t or f
-    }
-    else return "f"; // Returns f if nothing found
-}
-
-void updateTimer() {
-  char buff[5];
-  TFTscreen.fill(255, 0, 0);
-  TFTscreen.stroke(255,0,0);
-  TFTscreen.rect(0,0,TFTscreen.width(),20);
-  timeRemaining= String(timer.remaining()/1000);
-  timeRemaining.toCharArray(buff, 5);
-  TFTscreen.setTextSize(2);
-  TFTscreen.stroke(255,255,255);
-  TFTscreen.text(buff,0,0);
-  TFTscreen.setTextSize(1);
-}
-
-void checkTimer() {
-  bool timerCheck = timer.isFinished();
-  if (timerCheck || invalidRFID > 2 || invalidPassword > 2) {
-    for (int i=0; i<10; i++) {
-      tone(A0, 2000, 500);
-      delay(200);
-      tone(A0, 3000, 500);
-      delay(200);
-    }
-    updateScreen("INTRUDER ALERT!!!!!!", 20, 5, 20);
-    restartProgram();
+  if (buttonsPressed == false) {
+    binaryAnswer += "0";
+    buttonsPressed = true;
   }
 }
 
+//here we have a function used to recieve information from the slave arduino
+String recieveCommand() {
+  String command;
+  delay(100);
+  //if the connection is available (there is data on the connection)
+  if (mySerial.available()) {
+    char c = mySerial.read();  //gets one byte from serial buffer
+    command += c; //adds byte to a string
+  }
+  //if the string is not null return the string
+  if (command.length() > 0) {
+
+    return command; // Returns the string
+  }
+  else return "f"; // Returns f if nothing found
+}
+
+
+//update the timer function, this updates the timer on the led screen
+void updateTimer() {
+  char buff[5];
+  TFTscreen.fill(255, 0, 0);
+  TFTscreen.stroke(255, 0, 0);
+  TFTscreen.rect(0, 0, TFTscreen.width(), 20);
+  timeRemaining = String(timer.remaining() / 1000);
+  timeRemaining.toCharArray(buff, 5);
+  TFTscreen.setTextSize(2);
+  TFTscreen.stroke(255, 255, 255);
+  TFTscreen.text(buff, 0, 0);
+  TFTscreen.setTextSize(1);
+}
+
+//function to check if the time is up
+void checkTimer() {
+  //boolean value to store if the timer is up
+  bool timerCheck = timer.isFinished();
+  //if the timer is up, or either the RFID or the question have been done wrong 3 times then
+  if (timerCheck) {
+    //repeat an alarm
+    startAlarm();
+  }
+}
+
+void startAlarm() {
+  updateScreen("INTRUDER ALERT!!!!!!", 22, 5, 20);
+  for (int i = 0; i < 10; i++) {
+    tone(A0, 2000, 500);
+    delay(200);
+    tone(A0, 3000, 500);
+    delay(200);
+  }
+  //update the screen
+
+  //restart the program
+  restartProgram();
+}
+
+
+//this is the function for updating the text on the screen
+//it takes as input the text itself, length, x and y
 void updateScreen(String text, int textLen, int x, int y) {
+
   TFTscreen.background(255, 0, 0);
-  TFTscreen.stroke(255,255,255); // Clears the previous text
+  TFTscreen.stroke(255, 255, 255); // Clears the previous text
   char buf[textLen];
   text.toCharArray(buf, textLen);
   TFTscreen.text(buf, x, y);
 }
-
+//this function restarts the program at the end
+//it does so by resetting all variables and the screen and waiting a delay
 void restartProgram() {
   RFIDValid = "f";
   binaryAnswer = "";
   faceDetected = false;
-  invalidRFID = 0;
-  invalidPassword = 0;
+  buttonsPressed = true;
   delay(1000); // 5000 in final product
-  setup();
+  TFTscreen.background(255, 0, 0);
+  TFTscreen.stroke(255, 255, 255);
+  TFTscreen.text("Waiting for face", 5, 40); // Replaces the text with an empty string
 }
--- Prototyping/Screen_RFID/SlaveTest/SlaveTest.ino
+++ Prototyping/Screen_RFID/SlaveTest/SlaveTest.ino
@@ -1,56 +1,86 @@
 
-
+//including the library for soft serial
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(2, 3);
-String test;
-String validIDs[2] = {"1d 53 fe 9c ","bd a5 0 9d "};
-
+//initializing variables
+String input; // to store the input
+String validIDs[2] = {"1d 53 fe 9c ","bd a5 0 9d "}; // the valid RDIF UIDs
+int RFIDWrong; //how many times the RFID has been scanned wrong
+int questionWrong; // how many times the question has been answered wrong
 
 void setup() {
+  //establish connections
   Serial.begin(9600);
   mySerial.begin(38400);
 
 }
 
 void loop() {
-  test = ""; //clears variable for new input
+  input = ""; //clears variable for new input
   delay(10);
-
+  //if there is data
   if (mySerial.available()) {
+    //while data is comming, add the data to the input string
     while (mySerial.available()) {
       delay(10);  //small delay to allow input buffer to fill
 
       char c = mySerial.read();  //gets one byte from serial buffer
-      test += c;
+      input += c;
     } //makes the string readString
-    Serial.println(test);
-    if (test.length() > 9) {
-      testUID(test);
+  Serial.println(input);
+   //if the input length > 9 then it is for the RFID. therefor check the RDIF UID
+    if (input.length() > 9) {
+      testUID(input);
     }
+
+    //if it is not over 9 then it is for the question
     else{
-      if (test== "00101010"){
-        mySerial.write("t");
+      if (input == "00101010"){ // checks if the input is 42 in binary
+        mySerial.write("t"); // writes true
+        Serial.println("t");
       }
       else{
-        mySerial.write("f");
+        questionWrong ++; // increments the amount of wrong questions
+        returnFalse();
       }
     }
   }
-
-  // Serial.print(test);
 }
 
+//this funciton will test the inputted UID against the ones stored
 void testUID(String ID) {
   bool valid = false;
+  //for each correct ID compare them to the given ID
   for (String testID: validIDs) {
     if (ID == testID) {
       valid = true;
     }
   }
+  //if it is valid then write true
   if (valid) {
-    mySerial.write("t"); // Doesn't send t the first time for some reason??
+            Serial.println("t");
+
+    mySerial.write("t"); 
   }
   else {
+    //if it is not increment the amount of wrong RFIDs , and return 
+   RFIDWrong ++;
+   returnFalse();
+   }
+}
+
+
+void returnFalse(){
+  if ((RFIDWrong > 2) || (questionWrong > 2)){
+    RFIDWrong = 0;
+    questionWrong = 0;
+    mySerial.write("r");
+            Serial.println("r");
+
+  }
+  else{
     mySerial.write("f");
+            Serial.println("f");
+
   }
 }
